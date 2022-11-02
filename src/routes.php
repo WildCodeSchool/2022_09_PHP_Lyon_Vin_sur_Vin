c653420d2e7fa5d4648fa6db280e307f95fe17b9
busy-gnl <vallantjesse@live.com>
2022-11-02 20:23:15 +0100

    css partners done

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,8 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
+    // '' => ['HomeController', 'index',],
+    '' => ['WineController', 'showCard',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
@@ -24,5 +25,5 @@ return [
     'partners/show' => ['PartnerController', 'show', ['id']],
     'partners/add' => ['PartnerController', 'add',],
     'partners/delete' => ['PartnerController', 'delete',],
-    '' => ['WineController', 'showCard',]
+    // '' => ['WineController', 'showCard',]
 ];
