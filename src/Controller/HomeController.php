f95e4f4a0d0921b393ba294b545612c15b005688
CoquilledeLune <ljacobieski@gmail.com>
2022-11-12 13:32:24 +0100

    phone number can begin with a zero now

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,3 +12,4 @@ class HomeController extends AbstractController
         return $this->twig->render('Home/index.html.twig');
     }
 }
+/** Faire une fonction partner pour afficher la page partner */
