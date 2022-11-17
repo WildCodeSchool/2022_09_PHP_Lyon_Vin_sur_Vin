254e16ea5e63c2db7f251d825968f08e2b3e1d69
busy-gnl <vallantjesse@live.com>
2022-11-17 23:03:42 +0100

    fixed known bugs

--- src/Controller/AccountService.php
+++ src/Controller/AccountService.php
@@ -6,9 +6,7 @@ class AccountService extends AbstractController
 {
     public function logout()
     {
-        unset($_SESSION['admin_id']);
-        unset($_SESSION['user_id']);
-        unset($_SESSION['pro_id']);
+        session_destroy();
         header('Location: /');
     }
 
