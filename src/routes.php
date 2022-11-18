1bee1b19bb8bd151b7b2a4a561375dfdff9e58d6
busy-gnl <vallantjesse@live.com>
2022-11-18 22:33:06 +0100

    change AccountService to AccountController

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,5 @@
 <?php
 
-// list of accessible routes of your application, add every new route here
-// key : route to match
-// values : 1. controller name
-//          2. method name
-//          3. (optional) array of query string keys to send as parameter to the method
-// e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['WineController', 'showFavorites',],
     'items' => ['ItemController', 'index',],
